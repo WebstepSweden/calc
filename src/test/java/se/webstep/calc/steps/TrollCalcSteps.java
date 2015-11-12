317a56f75669a3de60c4daa8de2778c56e4bc158
Johan Rydström <johan@proburk.local>
2015-11-12 19:48:21 +0100

    Troll Scenarios!

--- src/test/java/se/webstep/calc/steps/TrollCalcSteps.java
+++ src/test/java/se/webstep/calc/steps/TrollCalcSteps.java
@@ -1,14 +1,17 @@
 package se.webstep.calc.steps;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cucumber.api.PendingException;
 import cucumber.api.java.sv.Givet;
+import cucumber.api.java.sv.Och;
 import cucumber.api.java.sv.Så;
 import se.webstep.calc.TrollCalc;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrollCalcSteps {
 
     private TrollCalc calc = new TrollCalc();
+    private Exception thrownException;
 
     @Givet("^jag nollställer trollminiräknaren$")
     public void i_reset_the_calculator() throws Throwable {
@@ -18,5 +21,53 @@ public class TrollCalcSteps {
     @Så("^ska trollresultatet vara (\\d+)$")
     public void result_should_be(int number) throws Throwable {
         assertThat(calc.result()).isEqualTo(number);
+    }
+
+    @Givet("^att jag adderar heltalen \"([^\"]*)\" och \"([^\"]*)\"$")
+    public void att_jag_anger_heltalen_och(String arg1, String arg2) throws Throwable {
+            calc.plus(arg1);
+            calc.plus(arg2);
+    }
+
+    @Så("^ska trollresultatet vara \"([^\"]*)\"$")
+    public void ska_trollresultatet_vara(String resultat) throws Throwable {
+        assertThat(calc.toString()).isEqualTo(resultat);
+    }
+
+    @Så("^ska trollresultatet vara \"([^\"]*)\" och (\\d+)$")
+    public void ska_trollresultatet_vara_och(String arg1, int arg2) throws Throwable {
+        assertThat(calc.toString()).isEqualTo(arg1);
+        assertThat(calc.result()).isEqualTo(arg2);
+    }
+
+    @Och("^\"([^\"]*)\" är större än (\\d+)$")
+    public void ärStörreÄn(String arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Givet("^att jag adderar heltalen \"([^\"]*)\" och \"([^\"]*)\" och \"([^\"]*)\" är större än (\\d+)$")
+    public void attJagAdderarHeltalenOchOchÄrStörreÄn(String arg0, String arg1, String arg2, int arg3) throws Throwable {
+        try {
+            calc.plus(arg0);
+            calc.plus(arg1);
+        } catch (Exception up) {
+            this.thrownException = up;
+        }
+    }
+
+    @Så("^skall \"([^\"]*)\" visas")
+    public void skallKastas(String expectedException) throws Throwable {
+        assertThat(thrownException.getMessage()).isEqualTo(expectedException);
+    }
+
+    @Givet("^att jag subtraherar heltalen \"([^\"]*)\" och \"([^\"]*)\"$")
+    public void attJagSubtraherarHeltalenOchMed(String arg0, String arg1) throws Throwable {
+      try {
+          calc.plus(arg0);
+          calc.minus(arg1);
+      } catch (Exception up) {
+          thrownException = up;
+      }
     }
 }
--- src/test/resources/se/webstep/calc/trollCalc.feature
+++ src/test/resources/se/webstep/calc/trollCalc.feature
@@ -3,6 +3,36 @@
 
 Egenskap: Beräkningar med trollminiräknaren
 
-  Scenario: När jag nollställer trollminiräknaren ska trollresultatet vara 0
-    Givet jag nollställer trollminiräknaren
-    Så ska trollresultatet vara 0
+   Scenario: När jag nollställer trollminiräknaren ska trollresultatet vara 0
+      Givet jag nollställer trollminiräknaren
+      Så ska trollresultatet vara 0
+
+   Scenario: Jag vill kunna addera två heltal.
+      Givet att jag adderar heltalen "ONE" och "THREE"
+      Så ska trollresultatet vara "MANY" och 4
+
+   Abstrakt Scenario: Jag vill kunna addera två heltal.
+      Givet att jag adderar heltalen "<tal1>" och "<tal2>"
+      Så ska trollresultatet vara "<sResultat>" och <iResultat>
+      Exempel:
+         | tal1           | tal2 | sResultat | iResultat |
+         | ONE            | ONE  | TWO       | 2         |
+         | ONE            | TWO  | THREE     | 3         |
+         | TWO            | ONE  | THREE     | 3         |
+         | TWO            | TWO  | MANY      | 4         |
+         | MANY-MANY-MANY | MANY | LOTS      | 16        |
+
+   Abstrakt Scenario: Troll kan inte addera två heltal vars resultat blir > 16
+      Givet att jag adderar heltalen "<tal1>" och "<tal2>" och "<Resultatet>" är större än 16
+      Så skall "<felmeddelandet>" visas
+      Exempel:
+         | tal1 | tal2 | Resultatet | felmeddelandet      |
+         | LOTS | ONE  | 17         | Trolls don't get 17 |
+
+   Abstrakt Scenario: Troll kan inte subtrahera två heltal vars resultat blir < 1
+      Givet att jag subtraherar heltalen "<tal1>" och "<tal2>"
+      Så skall "<felmeddelandet>" visas
+      Exempel:
+         | tal1 | tal2  | felmeddelandet                      |
+         | TWO  | TWO   | Troll don't think 0 is troll number |
+         | TWO  | THREE | Trolls don't get -1                 |
